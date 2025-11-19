ecaf9b275cab65c484f10e7505c0cad181d9ff2a
Ayush Pratap Singh <ayushdce2@gmail.com>
2025-11-19 17:48:16 +0530

    add

--- frontend/src/utility/axios.jsx
+++ frontend/src/utility/axios.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000"
+    // baseURL: "http://localhost:5000"
+
+    baseURL: "https://booking-mern-backend.onrender.com/"
 });
 
 export default API;
